8b59f98bd8e09444b4debd705512b881e5396328
iainconnor <iainconnor@gmail.com>
2018-03-27 22:40:42 -0400

    Adding onclicks.

--- www/NoTab.js
+++ www/NoTab.js
@@ -3,7 +3,6 @@ exports.browse = function(url, domainWhitelist, exitOnDone, closeSplashScreenOnL
     var inAppBrowser = cordova.InAppBrowser.open(url, '_blank', 'location=no,fullscreen=yes,hardwareback=yes');
     window.open = cordova.InAppBrowser.open;
     inErrorState = false;
-    doneLoad = false;
 
     document.addEventListener('online', function () {
         // If you're currently in an error state, reload the current page.
@@ -38,10 +37,7 @@ exports.browse = function(url, domainWhitelist, exitOnDone, closeSplashScreenOnL
             window.plugins.toast.hide();
         }
 
-        // Mark start loading.
-        doneLoad = false;
-
-        // Just in case we never finish loading, hide our splash and show the screen after a second.
+        // Just in case we never finish loading, hide our splash and show the screen after a few seconds.
         setTimeout(function() {
             onLoadStop();
         }, 3000);
@@ -51,62 +47,75 @@ exports.browse = function(url, domainWhitelist, exitOnDone, closeSplashScreenOnL
     });
 
     function onLoadStop() {
-        // If not already finished.
-        if ( !doneLoad ) {
-            // If you're currently in an error state, or you should close the splash screen on first load, close it now.
-            if ( typeof navigator !== "undefined" && typeof navigator.splashscreen !== "undefined" ) {
-                navigator.splashscreen.hide();
-            }
-
-            // No longer in an error state.
-            inErrorState = false;
-
-            // Override tabs with local links.
-            inAppBrowser.executeScript({
-                code:   ' \
-                    var domainWhiteListPattern = new RegExp("' + domainWhiteListPattern + '"); \
-                    \
-                    function shouldBeInternal(url, target) { \
-                        return (typeof target === "undefined" || (typeof target === "string" && target !== "_self")) && typeof url === "string" && (url.startsWith("#") || url.startsWith("/") || url.match(domainWhiteListPattern)); \
-                    } \
-                    \
-                    function noTab() { \
-                        var elements = document.querySelectorAll("a[target=\\\"_blank\\\"], form[target=\\\"_blank\\\"]"); \
-                        Array.prototype.forEach.call(elements, function(element, i){ \
-                            if (shouldBeInternal((element.action ? element.action : element.href), element.target)) { \
-                                element.target="_self"; \
-                            } \
-                        }); \
-                    } \
-                    \
-                    noTab(); \
-                    \
-                    var _windowOpen = window.open; \
-                    window.open = function(url, target, features) { \
-                        if (shouldBeInternal(url, target)) { \
-                            _windowOpen(url, "_self", features); \
-                        } else { \
-                            _windowOpen(url, target, features); \
-                        } \
-                    }; \
-                    \
-                    (function(open) { \
-                        XMLHttpRequest.prototype.open = function () { \
-                            this.addEventListener("readystatechange", function () { \
-                                if (this.readyState == 4) { \
-                                    setTimeout(function() { \
-                                        noTab(); \
-                                    }, 800); \
-                                } \
-                            }, false); \
-                            open.apply(this, arguments); \
-                        }; \
-                    })(XMLHttpRequest.prototype.open);'
-            });
+        // If you're currently in an error state, or you should close the splash screen on first load, close it now.
+        if ( typeof navigator !== "undefined" && typeof navigator.splashscreen !== "undefined" ) {
+            navigator.splashscreen.hide();
         }
 
-        // Now done.
-        doneLoad = true;
+        // No longer in an error state.
+        inErrorState = false;
+
+        // Override tabs with local links.
+        inAppBrowser.executeScript({
+            code:   ' \
+                var domainWhiteListPattern = new RegExp("' + domainWhiteListPattern + '"); \
+                \
+                function shouldBeInternal(url, target) { \
+                    return (typeof target === "undefined" || (typeof target === "string" && target !== "_self")) && typeof url === "string" && (url.startsWith("#") || url.startsWith("/") || url.match(domainWhiteListPattern)); \
+                } \
+                \
+                document.onclick = function (e) { \
+                    e = e || window.event; \
+                    var element = e.target || e.srcElement; \
+                    \
+                    if (element.tagName.toUpperCase() !== "A") { \
+                        element = element.closest("a"); \
+                    } \
+                    \
+                    if (typeof element !== "undefined" && element.tagName.toUpperCase() == "A") { \
+                        if (shouldBeInternal(element.href, element.target)) { \
+                            window.open(element.href, "_self"); \
+                            \
+                            return false; \
+                        } \
+                    } \
+                }; \
+                \
+                function noTab() { \
+                    var elements = document.querySelectorAll("a[target=\\\"_blank\\\"], form[target=\\\"_blank\\\"]"); \
+                    Array.prototype.forEach.call(elements, function(element, i){ \
+                        if (shouldBeInternal((element.action ? element.action : element.href), element.target)) { \
+                            element.target="_self"; \
+                        } \
+                    }); \
+                } \
+                \
+                setTimeout(function() { \
+                    noTab(); \
+                }, 1000); \
+                \
+                var _windowOpen = window.open; \
+                window.open = function(url, target, features) { \
+                    if (shouldBeInternal(url, target)) { \
+                        _windowOpen(url, "_self", features); \
+                    } else { \
+                        _windowOpen(url, target, features); \
+                    } \
+                }; \
+                \
+                (function(open) { \
+                    XMLHttpRequest.prototype.open = function () { \
+                        this.addEventListener("readystatechange", function () { \
+                            if (this.readyState == 4) { \
+                                setTimeout(function() { \
+                                    noTab(); \
+                                }, 1000); \
+                            } \
+                        }, false); \
+                        open.apply(this, arguments); \
+                    }; \
+                })(XMLHttpRequest.prototype.open);'
+        });
     }
 
     inAppBrowser.addEventListener('loadstop', function () {
